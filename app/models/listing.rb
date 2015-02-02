c47b729074838ce646aadcb1f2c6a4c6231bd6d4
rickmangun <rickmangun@gmail.com>
2015-02-02 10:51:55 -0600

    alex addition github

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,7 @@ class Listing < ActiveRecord::Base
 	
     has_attached_file :image, :styles => { :medium => "200x",:thumb => "100x100>" }, :default_url => "default.jpg",
        	:storage => :dropbox,			
-    	:dropbox_credentials => Rails.root.join("config/dropbox.yml")								
+    	:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    	:path => ":style/:id_:filename"								
     validates_attachment_content_type :image, :content_type => %w(image/jpg image/jpeg image/png image/gif)
 end		
